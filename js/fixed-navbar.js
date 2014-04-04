0d74e706f877badfb7b699c1721f2ef7de726615
scott-linder <scott.b.linder@wmich.edu>
2014-04-04 14:41:00 -0400

    Added fixed navbar on scroll (depends on jQuery).

---
+++ js/fixed-navbar.js
@@ -0,0 +1,13 @@
+/* Fixed navbar on scroll. */
+$(window).load(function () {
+    navbar_fixed = $('#navbar').clone().addClass('navbar-fixed');
+    update_navbar_fixed = function () {
+        if ($(this).scrollTop() > $('#navbar').offset().top) {
+            $('body').append(navbar_fixed);
+        } else {
+            navbar_fixed.remove();
+        }
+    };
+    $(window).scroll(update_navbar_fixed);
+    $(window).resize(update_navbar_fixed);
+});
